8ad0b1649d0260f3b564212437cdd374a41ccd5f
Nour <mohamednour25@gmail.com>
2024-09-14 14:15:03 +0300

    Initial Commit

---
+++ BuyNowView/BuyNowViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuyNowViewApp.swift
+//  BuyNowView
+//
+//  Created by administrator on 14/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct BuyNowViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
